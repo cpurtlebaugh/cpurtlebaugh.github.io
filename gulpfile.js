04cbf20615aec1bbd832531fad235d48e8f0b5c9
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2016-01-16 16:39:52 -0800

    added devicon svgs

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,12 @@ var gulp = require('gulp');
 
 // Include Our Plugins
 var jshint = require('gulp-jshint');
-var sass = require('gulp-sass');
+var sass   = require('gulp-sass');
+var less   = require('gulp-less');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
-var reload = require('gulp-livereload');
+var livereload = require('gulp-livereload');
 
 // Lint Task
 gulp.task('lint', function() {
@@ -33,11 +34,21 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('dist'));
 });
 
+gulp.task('less', function() {
+  gulp.src('less/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('css'))
+    .pipe(livereload());
+});
+
 // Watch Files For Changes
 gulp.task('watch', function() {
+    livereload.listen();
+    gulp.watch('less/*.less', ['less']);
     gulp.watch('js/*.js', ['lint', 'scripts']);
     gulp.watch('scss/*.scss', ['sass']);
 });
 
+
 // Default Task
-gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
+gulp.task('default', ['lint', 'sass', 'scripts', 'watch', 'less']);
