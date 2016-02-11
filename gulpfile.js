f22cd9030340f2ab38ed8726e53baa57c3648576
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2016-02-10 23:43:07 -0800

    updated package.json file and gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,34 @@
 // Include gulp
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 
-// Include Our Plugins
+// Include Plugins
 var jshint = require('gulp-jshint');
-var sass   = require('gulp-sass');
-var less   = require('gulp-less');
+var minifyCss = require('gulp-minify-css');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var livereload = require('gulp-livereload');
+var deploy = require('gulp-gh-pages');
+
+// create a default task and just log a message
+gulp.task('default', function() {
+  return gutil.log('Gulp is running!')
+});
+
+// gulp.task('default', ['watch', 'build-js', 'minify-css']);
+
+// Watch Task
+gulp.task('watch', function(){
+    gulp.watch('js/*js', ['jshint']);
+});
+
+// JS Hint Task
+gulp.task('jshint', function() {
+    return gulp.src('js/*js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+});
 
 // Lint Task
 gulp.task('lint', function() {
@@ -17,38 +37,28 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter('default'));
 });
 
-// Compile Our Sass
-gulp.task('sass', function() {
-    return gulp.src('scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('css'));
-});
-
 // Concatenate & Minify JS
-gulp.task('scripts', function() {
+gulp.task('build-js', function() {
     return gulp.src('js/*.js')
         .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('all.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('dist'));
+        .pipe(rename('all.min.js'))
+        .pipe(gulp.dest('dist'))
 });
 
-gulp.task('less', function() {
-  gulp.src('less/*.less')
-    .pipe(less())
-    .pipe(gulp.dest('css'))
-    .pipe(livereload());
+// Concatengate & Minify CSS
+gulp.task('minify-css', function() {
+  return gulp.src('css/*.css')
+    .pipe(concat('all.min.css'))
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'));
 });
 
-// Watch Files For Changes
-gulp.task('watch', function() {
-    livereload.listen();
-    gulp.watch('less/*.less', ['less']);
-    gulp.watch('js/*.js', ['lint', 'scripts']);
-    gulp.watch('scss/*.scss', ['sass']);
+// Deploy dist to GH pages
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
 });
 
 
 // Default Task
-gulp.task('default', ['lint', 'sass', 'scripts', 'watch', 'less']);
